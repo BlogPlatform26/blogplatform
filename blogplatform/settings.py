27e1740b4f63c60f47e4c01c8de5794483e7585c
BlogPlatform26 <blogplatform.site@gmail.com>
2026-06-15 19:32:08 +0200

    Enable WhiteNoise for static files

--- blogplatform/settings.py
+++ blogplatform/settings.py
@@ -117,6 +117,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "blogplatform.csp.ContentSecurityPolicyMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
